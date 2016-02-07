8e0c5ba703483047baf513979ca0e8664d8964d6
Louis Vichy <lgvichy@gmail.com>
2016-02-07 08:17:22 -0800

    cool with capital one now

--- server/lab.js
+++ server/lab.js
@@ -92,6 +92,7 @@ function initTransaction(req, res, next) {
   });
 }
 
+
 function makeTransaction(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
